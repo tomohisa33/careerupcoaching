c931acef67a4696cb24a3036a0abe69f6acc223b
tomohisa3 <53322243+tomohisa33@users.noreply.github.com>
2021-11-04 08:53:48 +0900

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$('a[href^="#"]').click(function() {
+    // スクロールの速度
+    let speed = 400; // ミリ秒で記述
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
